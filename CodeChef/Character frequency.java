ca9e22b844a0c1b9e5d311ac6dcbc6c8efbd2e05
ROHINTH R <rohinth076@gmail.com>
2022-01-26 14:43:03 +0530

    Create Character frequency.java

---
+++ CodeChef/Character frequency.java
@@ -0,0 +1,76 @@
+/*
+Question : https://www.codechef.com/CCMB2022/problems/FREQ
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+class Solution{
+	static FastScanner fs  = new FastScanner();
+	static PrintWriter out = new PrintWriter(System.out);
+	static void output(){
+       char[] a = fs.next().toCharArray();
+       int n = a.length;
+       Map<Character,Integer> m = new HashMap<>();
+       int[] b = new int[n];
+       for(int i=0;i<n;i++){
+           m.merge(a[i],1,Integer::sum);
+           b[i] = m.get(a[i])-1;
+       }
+       int q = fs.nextInt();
+       while(q-->0){
+           int in = fs.nextInt()-1;
+           out.println(b[in]);
+       }
+       
+	}
+	public static void main(String[] args) {
+		
+		int T = 1;
+	    //T=fs.nextInt();
+		for (int tt=0; tt<T; tt++) {
+			output();
+			out.flush();
+		}
+	}
+
+	static void sort(int[] a) {
+		ArrayList<Integer> l=new ArrayList<>();
+		for (int i:a) l.add(i);
+		Collections.sort(l);
+		for (int i=0; i<a.length; i++) a[i]=l.get(i);
+	}
+	
+	static class FastScanner {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer("");
+		String next() {
+			while (!st.hasMoreTokens())
+				try {
+					st=new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			return st.nextToken();
+		}
+		
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		int[] readArray(int n) {
+			int[] a=new int[n];
+			for (int i=0; i<n; i++) a[i]=nextInt();
+			return a;
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+}
