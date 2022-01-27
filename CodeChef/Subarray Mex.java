fb50cab87a2596d6d4c3a2f5f7a5d378a643192b
ROHINTH R <rohinth076@gmail.com>
2022-01-27 09:16:13 +0530

    Create Subarray Mex.java

---
+++ CodeChef/Subarray Mex.java
@@ -0,0 +1,94 @@
+/*
+Question : https://www.codechef.com/START23B/problems/SUBMEX
+*/
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+class Solution{
+	static FastScanner fs  = new FastScanner();
+	static PrintWriter out = new PrintWriter(System.out);
+	static void output(){
+       int n = fs.nextInt(),l = fs.nextInt(),x = fs.nextInt();
+       if(x > l){
+           out.println(-1);
+           return;
+       }
+       int j = 0;
+       while(j<n){
+           int i =0,k=0;
+           while(j<n && i<l){
+               if(k != x){
+                  out.print(k+" ");
+                  i++;
+                  j++;
+               }
+               k++;
+           }
+       }
+       out.println();
+        
+       
+	}
+	public static void main(String[] args) {
+		
+		int T = 1;
+	    T=fs.nextInt();
+		for (int tt=0; tt<T; tt++) {
+			output();
+			out.flush();
+		}
+	}
+
+	static void sort(int[] a) {
+		ArrayList<Integer> l=new ArrayList<>();
+		for (int i:a) l.add(i);
+		Collections.sort(l);
+		for (int i=0; i<a.length; i++) a[i]=l.get(i);
+	}
+	
+	static class FastScanner {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer("");
+		String next() {
+			while (!st.hasMoreTokens())
+				try {
+					st=new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			return st.nextToken();
+		}
+		
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		int[] readArray(int n) {
+			int[] a=new int[n];
+			for (int i=0; i<n; i++) a[i]=nextInt();
+			return a;
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		String nextLine()
+        {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+	}
+}
