83d10d527debed011742e5219fc5084896d27cfa
ROHINTH R <rohinth076@gmail.com>
2022-01-29 12:09:58 +0530

    Create Majority_Wins.java

---
+++ CodeChef/Majority_Wins.java
@@ -0,0 +1,86 @@
+/*
+Question : https://www.codechef.com/CDKR2022/problems/MEENA_008
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+class Solution{
+	static FastScanner fs  = new FastScanner();
+	static PrintWriter out = new PrintWriter(System.out);
+	static void output(){
+       int n = fs.nextInt();
+       int[] a = fs.readArray(n);
+       Map<Integer,Integer> m =new HashMap<>();
+       for(int i:a){
+           m.merge(i,1,Integer::sum);
+       }
+       for(Map.Entry<Integer,Integer> i:m.entrySet()){
+           if(i.getValue() > n/2){
+               out.println(i.getKey());
+               return;
+           }
+       }
+       out.println("NOTA");
+	}
+	public static void main(String[] args) {
+		
+		int T = 1;
+	   T=fs.nextInt();
+		for (int tt=0; tt<T; tt++) {
+			output();
+			out.flush();
+		}
+	}
+
+	static void sort(int[] a) {
+		ArrayList<Integer> l=new ArrayList<>();
+		for (int i:a) l.add(i);
+		Collections.sort(l);
+		for (int i=0; i<a.length; i++) a[i]=l.get(i);
+	}
+	
+	static class FastScanner {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer("");
+		String next() {
+			while (!st.hasMoreTokens())
+				try {
+					st=new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			return st.nextToken();
+		}
+		
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		int[] readArray(int n) {
+			int[] a=new int[n];
+			for (int i=0; i<n; i++) a[i]=nextInt();
+			return a;
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		String nextLine()
+        {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+	}
+}
