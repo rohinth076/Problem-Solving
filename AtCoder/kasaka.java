5b1c88800365834d2abcef474c0313f95fa4ccc8
ROHINTH R <rohinth076@gmail.com>
2022-01-30 19:20:25 +0530

    Create kasaka.java

---
+++ AtCoder/kasaka.java
@@ -0,0 +1,129 @@
+/*
+Question : https://atcoder.jp/contests/abc237/tasks/abc237_c
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class C{
+    static QuickReader fs = new QuickReader();
+    static PrintWriter out = new PrintWriter(System.out);
+    static  boolean isPalindrome(char[] a,int s,int e){
+        for(int i=s,j=e;i<j;i++,j--)
+            if(a[i] != a[j])
+                return false;
+        return true;
+    }
+    static void output() {
+        String s = fs.next();
+        char[] a = s.toCharArray();
+        int n = a.length;
+        boolean flag = false;
+        if(isPalindrome(a,0,n-1)){
+            flag = true;
+        }else {
+            int start = 0, end = n - 1;
+            while (start < n && a[start] == 'a') {
+                start++;
+            }
+            while (end >= 0 && a[end] == 'a') {
+                end--;
+            }
+            int d = n - end - 1;
+            int dif = Math.abs(d - start);
+            if (d > start) {
+                if(isPalindrome(a,0,n-1-dif))flag = true;
+            }
+        }
+        out.println((flag)?"Yes":"No");
+    }
+
+    public static void main(String[] args) {
+
+        int T = 1;
+        //T = fs.nextInt();
+        for (int tt = 0; tt < T; tt++) {
+            output();
+            out.flush();
+        }
+    }
+
+    static void printArray(int[] a) {
+        for (int i : a) out.print(i + " ");
+        out.println();
+    }
+
+    static void printlnArray(int[] a) {
+        for (int i : a) out.println(i);
+    }
+
+    static void printMatrix(int[][] a) {
+        for (int[] i : a)
+            printArray(i);
+    }
+
+    static void sortA(int[] a) {
+        ArrayList<Integer> l = new ArrayList<>();
+        for (int i : a) l.add(i);
+        Collections.sort(l);
+        for (int i = 0; i < a.length; i++) a[i] = l.get(i);
+    }
+
+    static void sortD(int[] a) {
+        ArrayList<Integer> l = new ArrayList<>();
+        for (int i : a) l.add(i);
+        Collections.sort(l, (i, j) -> j - i);
+        for (int i = 0; i < a.length; i++) a[i] = l.get(i);
+    }
+
+    static class QuickReader {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        int[] readIntArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        long[] readLongArray(int n) {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++) a[i] = nextLong();
+            return a;
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
+
