ee1e6662ccfd1b76169a1045384f6684d3c05c3c
ROHINTH R <rohinth076@gmail.com>
2022-01-30 19:17:38 +0530

    Update and rename 1.java to Not Overflow.java

---
+++ AtCoder/Not Overflow.java
@@ -0,0 +1,106 @@
+/*
+Question : https://atcoder.jp/contests/abc237/tasks/abc237_a
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+class A {
+    static QuickReader fs = new QuickReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    static void output() {
+         long min =(long) -Math.pow(2,31),max =(long) Math.pow(2,31)-1;
+         long n = fs.nextLong();
+         out.println((n>=min && n<=max)?"Yes":"No");
+    }
+
+    public static void main(String[] args) {
+
+        int T = 1;
+        //T = fs.nextInt();
+        for (int tt = 0; tt < T; tt++) {
+            output();
+            out.flush();
+        }
+    }
+
+    static void printArray(int[] a) {
+        for (int i : a) out.print(i + " ");
+        out.println();
+    }
+
+    static void printlnArray(int[] a) {
+        for (int i : a) out.println(i);
+    }
+
+    static void printMatrix(int[][] a) {
+        for (int[] i : a)
+            printArray(i);
+    }
+
+    static void sortA(int[] a) {
+        ArrayList<Integer> l = new ArrayList<>();
+        for (int i : a) l.add(i);
+        Collections.sort(l);
+        for (int i = 0; i < a.length; i++) a[i] = l.get(i);
+    }
+
+    static void sortD(int[] a) {
+        ArrayList<Integer> l = new ArrayList<>();
+        for (int i : a) l.add(i);
+        Collections.sort(l, (i, j) -> j - i);
+        for (int i = 0; i < a.length; i++) a[i] = l.get(i);
+    }
+
+    static class QuickReader {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        int[] readIntArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        long[] readLongArray(int n) {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++) a[i] = nextLong();
+            return a;
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
+
