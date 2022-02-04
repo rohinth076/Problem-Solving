5d77561b21890aa543d2a9aac27bc0101cc51975
ROHINTH R <rohinth076@gmail.com>
2022-02-04 11:26:48 +0530

    Create Sum of Round Numbers.java

---
+++ CodeForces/Sum of Round Numbers.java
@@ -0,0 +1,120 @@
+
+/*
+Question : https://codeforces.com/problemset/problem/1352/A
+
+
+* */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class A5 {
+    static QuickReader fs = new QuickReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    static void output() {
+            int n = fs.nextInt();
+            List<Integer> l = new ArrayList<>();
+            int d = 10,i=0;
+            while(n > 0){
+                int r = n % d;
+                int a = r*(int)Math.pow(d,i);
+                if(a != 0) l.add(a);
+                i++;
+                n = n / d;
+            }
+            out.println(l.size());
+            for(Integer j:l)
+                out.print(j+" ");
+            out.println();
+    }
+
+    public static void main(String[] args) {
+
+        int T = 1;
+        T = fs.nextInt();
+        for (int tt = 0; tt < T; tt++) {
+            output();
+            out.flush();
+        }
+    }
+
+    static void printArray(int[] a) {
+        for (int i : a) out.print(i + " ");
+        out.println();
+    }
+
+    static void printlnArray(int[] a) {
+        for (int i : a) out.println(i);
+    }
+
+    static void printMatrix(int[][] a) {
+        for (int[] i : a)
+            printArray(i);
+    }
+
+    static void sortA(int[] a) {
+        ArrayList<Integer> l = new ArrayList<>();
+        for (int i : a) l.add(i);
+        Collections.sort(l);
+        for (int i = 0; i < a.length; i++) a[i] = l.get(i);
+    }
+
+    static void sortD(int[] a) {
+        ArrayList<Integer> l = new ArrayList<>();
+        for (int i : a) l.add(i);
+        Collections.sort(l, (i, j) -> j - i);
+        for (int i = 0; i < a.length; i++) a[i] = l.get(i);
+    }
+
+    static class QuickReader {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        int[] readIntArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        long[] readLongArray(int n) {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++) a[i] = nextLong();
+            return a;
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
