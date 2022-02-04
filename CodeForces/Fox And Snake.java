0bbca01a5bd216cfe47763e05ec89b4c3648daf4
ROHINTH R <rohinth076@gmail.com>
2022-02-04 11:27:15 +0530

    Create Fox And Snake.java

---
+++ CodeForces/Fox And Snake.java
@@ -0,0 +1,127 @@
+/*
+Question : https://codeforces.com/problemset/problem/510/A
+
+* */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class A6 {
+    static QuickReader fs = new QuickReader();
+    static PrintWriter out = new PrintWriter(System.out);
+    static void pp1(int n){
+        for(int i=1;i<=n;i++)
+            out.print("#");
+        out.println();
+    }
+    static void pp2(int n){
+        for(int i=1;i<=n;i++)
+            out.print(".");
+    }
+    static void output() {
+        int r = fs.nextInt(),c = fs.nextInt();
+        for(int i=1;i<=r;i++){
+            if(i%2 != 0){
+                pp1(c);
+            }else{
+                if((i/2) %2 != 0){
+                    pp2(c-1);
+                    pp1(1);
+
+                }else{
+                    out.print("#");
+                    pp2(c-1);
+                    out.println();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int T = 1;
+        //T = fs.nextInt();
+        for (int tt = 0; tt < T; tt++) {
+            output();
+            out.flush();
+        }
+    }
+
+    static void printArray(int[] a) {
+        for (int i : a) out.print(i + " ");
+        out.println();
+    }
+
+    static void printlnArray(int[] a) {
+        for (int i : a) out.println(i);
+    }
+
+    static void printMatrix(int[][] a) {
+        for (int[] i : a)
+            printArray(i);
+    }
+
+    static void sortA(int[] a) {
+        ArrayList<Integer> l = new ArrayList<>();
+        for (int i : a) l.add(i);
+        Collections.sort(l);
+        for (int i = 0; i < a.length; i++) a[i] = l.get(i);
+    }
+
+    static void sortD(int[] a) {
+        ArrayList<Integer> l = new ArrayList<>();
+        for (int i : a) l.add(i);
+        Collections.sort(l, (i, j) -> j - i);
+        for (int i = 0; i < a.length; i++) a[i] = l.get(i);
+    }
+
+    static class QuickReader {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        int[] readIntArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        long[] readLongArray(int n) {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++) a[i] = nextLong();
+            return a;
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
