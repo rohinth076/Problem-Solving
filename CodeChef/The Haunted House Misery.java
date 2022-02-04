b9ebf8e0dc169b367d37fbc96e25476dbaab63ea
ROHINTH R <rohinth076@gmail.com>
2022-02-04 09:50:17 +0530

    Create The Haunted House Misery.java

---
+++ CodeChef/The Haunted House Misery.java
@@ -0,0 +1,107 @@
+/*
+Question : https://www.codechef.com/CBND2022/problems/HAUNTEDHOUSE/
+*/
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+class Solution{
+    static QuickReader fs  = new QuickReader();
+    static PrintWriter out = new PrintWriter(System.out);
+    static void output(){
+        int n = fs.nextInt();
+        int[] a = new int[n];
+        int max = 0;
+        Map<Integer,Integer> m = new HashMap<>();
+        for(int i=0;i<n;i++){
+            a[i] = fs.nextInt();
+            m.merge(a[i],1,Integer::sum);
+            max = Math.max(max,m.get(a[i]));
+        }
+        int c = 0;
+        for(Map.Entry<Integer,Integer> i:m.entrySet())
+            if(max == i.getValue())
+                c++;
+        int ans = (n-max-c+1)/(max-1);
+        
+        out.println(ans);
+    }
+    public static void main(String[] args) {
+
+        int T = 1;
+        T=fs.nextInt();
+        for (int tt=0; tt<T; tt++) {
+            output();
+            out.flush();
+        }
+    }
+
+    /* The template starts here */
+    static void printArray(int[] a){
+        for(int i:a)out.print(i+" ");
+        out.println();
+    }
+
+    static void printlnArray(int[] a){
+        for(int i:a)out.println(i);
+    }
+
+    static void printMatrix(int[][] a){
+        for(int[] i:a)
+            printArray(i);
+    }
+
+    static void sort(int[] a) {
+        ArrayList<Integer> l=new ArrayList<>();
+        for (int i:a) l.add(i);
+        Collections.sort(l);
+        for (int i=0; i<a.length; i++) a[i]=l.get(i);
+    }
+
+    static class QuickReader {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer("");
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st=new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        int[] readIntArray(int n) {
+            int[] a=new int[n];
+            for (int i=0; i<n; i++) a[i]=nextInt();
+            return a;
+        }
+        long[] readLongArray(int n) {
+            long[] a=new long[n];
+            for (int i=0; i<n; i++) a[i]=nextLong();
+            return a;
+        }
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+        String nextLine()
+        {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
